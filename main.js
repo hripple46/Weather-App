5acd4052fe386d5e36aff23013ff4e1f8b7a683f
Henry Ripple <hripple46@gmail.com>
2022-12-15 15:42:34 -0600

    5-day forcast is displayed with average temperature for each day shown

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const weatherLabels = [
   "Air Pressure",
   "Humidity",
 ];
-
+//returns current weather
 function getWeather(userInput) {
   fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -20,13 +20,13 @@ function getWeather(userInput) {
       return currentWeather;
     })
     .then(function (currentWeather) {
-      console.log(currentWeather);
       let weatherResult = currentWeather.main;
       let weatherName = currentWeather.name;
       //displayWeather(weatherResult);
       displayWeather(weatherResult, weatherName);
     });
 }
+//returns current weather if user inputs a zipcode
 function getWeatherByZip(userInput) {
   fetch(
     "https://api.openweathermap.org/data/2.5/weather?zip=" +
@@ -39,12 +39,31 @@ function getWeatherByZip(userInput) {
       return currentWeather;
     })
     .then(function (currentWeather) {
-      console.log(currentWeather);
       let weatherResult = currentWeather.main;
       let weatherName = currentWeather.name;
       //displayWeather(weatherResult);
       displayWeather(weatherResult, weatherName);
     });
+}
+
+function getForecast(userInput) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      userInput +
+      "&appid=17312825d27592a56aa8d8b5b3280657&units=imperial",
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      let forecast = response.json();
+      return forecast;
+    })
+    .then(function (forecast) {
+      let weatherResult = forecast.list;
+      displayDailyForecast(weatherResult);
+
+      return weatherResult;
+    })
+    .then(function (weatherResult) {});
 }
 
 let userSubmit = document.querySelector("#button");
@@ -56,18 +75,19 @@ userSubmit.addEventListener("click", () => {
     getWeatherByZip(userInputValue);
   } else {
     getWeather(userInputValue);
+    getForecast(userInputValue);
   }
 });
 
 function displayWeather(x, y) {
   let index = 0;
   document.querySelector("#result").innerText = "";
+  document.querySelector("#forecastOverview").innerText = "";
   let locationName = document.createElement("h1");
   locationName.textContent = y;
   document.querySelector("#result").appendChild(locationName);
   for (let i in x) {
     let newDiv = document.createElement("div");
-    console.log(`${[i]}`);
     if (`${[i]}`.includes("temp") || `${[i]}`.includes("feel")) {
       newDiv.innerText = weatherLabels[index] + ": " + `${x[i]}` + "\u2109";
     } else {
@@ -75,5 +95,40 @@ function displayWeather(x, y) {
     }
     document.querySelector("#result").appendChild(newDiv);
     index += 1;
+  }
+}
+function displayDailyForecast(x) {
+  let currentDay = "";
+  for (let i in x) {
+    let weatherDetails = "";
+
+    console.log(`${x[i]}`);
+    for (let j in x[i]) {
+      if (`${j}` == "main") {
+        for (let z in x[i][j]) {
+          //console.log(`${z}: ` + `${x[i][j][z]}`);
+          if (`${z}` == "temp") {
+            weatherDetails = "Average Temp: " + `${x[i][j][z]}` + "\u2109";
+          }
+        }
+      }
+      if (`${j}` == "dt_txt") {
+        let splitDate = `${x[i][j]}`.split(" ");
+        if (currentDay == splitDate[0]) {
+          continue;
+        } else {
+          currentDay = `${splitDate[0]}`;
+          let date = document.createElement("h1");
+          date.innerText = currentDay;
+          console.log(x[i]);
+          document.querySelector("#forecastOverview").appendChild(date);
+          console.log(weatherDetails);
+
+          let children = document.createElement("p");
+          children.innerText = weatherDetails;
+          document.querySelector("#forecastOverview").appendChild(children);
+        }
+      }
+    }
   }
 }
